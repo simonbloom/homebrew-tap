ab05ef35cb87f31490c869a6510af80bd0b0c7ec
Simon Bloom <simon.bloom@gmail.com>
2026-01-02 14:38:41 +0000

    Bump to v1.0.25

--- Casks/progress11.rb
+++ Casks/progress11.rb
@@ -1,5 +1,5 @@
 cask "progress11" do
-  version "1.0.24"
+  version "1.0.25"
   sha256 :no_check
 
   url "https://github.com/simonbloom/gittool-releases/releases/download/v#{version}/Progress11.dmg"
